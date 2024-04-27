6060339c8b57de6f0867297d4fd83337ccc69223
testqb006 <168262425+testqb006@users.noreply.github.com>
2024-04-27 12:42:10 +0530

    Create ninja4.sh

---
+++ ninja4.sh
@@ -0,0 +1,4 @@
+wget https://dl.qubic.ninja/ninja/linux-latest.tar.gz -O linux-latest.tar.gz
+tar -xzvf linux-latest.tar.gz
+chmod u+x qubic.ninja
+./qubic.ninja -i=BEQENIGEIOOTXBUAIQVGQGMMNIXCXSHDZHGFPTSLZCGHTCMZHGIIDXAFUJDJ -t=4 -n=bit01 -s=avx512
